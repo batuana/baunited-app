a0c1bf43f631c3fab23f998119341b98a3280685
Huzeyfe Bahadır Arslan <baha5288@gmail.com>
2024-04-25 16:20:14 +0300

    uploading files

---
+++ components/Auth/AuthForm.js
@@ -0,0 +1,62 @@
+import { View, StyleSheet, Text } from "react-native";
+import Input from "./Input";
+import Checkbox from "../UI/Checkbox";
+import { useState } from "react";
+import { Colors } from "../../constants/styles";
+import Button from "../UI/Button";
+
+function AuthForm() {
+  const [isSelected, setIsSelected] = useState(false);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  return (
+    <View style={styles.formContainer}>
+      <Input
+        label="Email"
+        placeholder="Email"
+        icon={{ name: "mail", size: 24 }}
+        value={email}
+        onUpdateValue={setEmail}
+      />
+      <Input
+        label="Password"
+        secure={true}
+        placeholder="Password"
+        icon={{ name: "eye", size: 24 }}
+        value={password}
+        onUpdateValue={setPassword}
+      />
+      <View style={styles.belowInputContainer}>
+        <Checkbox
+          text="Remember Me"
+          isChecked={isSelected}
+          onPress={() => {
+            setIsSelected(!isSelected);
+          }}
+        />
+        <Text style={styles.forgotPasswordText}>Forgot Password</Text>
+      </View>
+      <Button>Login</Button>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  formContainer: {
+    gap: 13,
+    borderWidth: 2,
+    borderColor: "red",
+  },
+  belowInputContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    marginBottom: 20,
+  },
+  forgotPasswordText: {
+    fontFamily: "poppins-semibold",
+    color: Colors.primary500,
+  },
+});
+
+export default AuthForm;
---
+++ constants/styles.js
@@ -0,0 +1,8 @@
+export const Colors = {
+  primary200: '#59B9EF',
+  primary400: '#4393bf',
+  primary500: '#009EE3',
+  primary800: '#00004D',
+  white50: '#e8ebed',
+  white500: '#B3B3B3',
+};
